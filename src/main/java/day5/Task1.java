37f838f709f8f7a2ce027afc6bd47af55301dd38
Aleksandr <ShurikElabuga@yandex.ru>
2023-02-17 15:49:53 +0300

    complite day5 tasks

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,35 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        Car car1 = new Car();
+        car1.setModel("ВАЗ - 2101");
+        car1.setColor("Синий");
+        car1.setYear(1982);
+        System.out.println("Модель автомобиля: " + car1.getModel());
+        System.out.println("Цвет автомобиля: " + car1.getColor());
+        System.out.println("Года выпуска: " + car1.getYear());
+    }
+}
+class Car {
+    private String model;
+    private String color;
+    private int year;
+    public void setModel(String newModel) {
+         model = newModel;
+    }
+    public void setColor(String newColor) {
+        color = newColor;
+    }
+    public void setYear(int newYear) {
+        year = newYear;
+    }
+    public String getModel() {
+        return model;
+    }
+    public String getColor() {
+        return color;
+    }
+    public int getYear() {
+        return year;
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,28 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
-
+        Motorbike bike1 = new Motorbike("Урал", "Черный", 1985);
+        System.out.println("Модель: " + bike1.getModel());
+        System.out.println("Цвет: " + bike1.getColor());
+        System.out.println("Год выпуска: " + bike1.getYear());
+    }
+}
+class Motorbike {
+    private String color;
+    private String model;
+    private int year;
+    public Motorbike(String model, String color, int year) {
+        this.model = model;
+        this.color = color;
+        this.year = year;
+    }
+    public String getModel() {
+        return model;
+    }
+    public String getColor() {
+        return color;
+    }
+    public int getYear() {
+        return year;
     }
 }
