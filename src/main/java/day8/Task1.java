f7eca3c5ff0ecef229e353e3276c7c293823515a
Aleksandr <ShurikElabuga@yandex.ru>
2023-02-20 19:42:02 +0300

    complite day8 tasks

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,23 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+     String str = "";
+     long startTime = System.currentTimeMillis();
+     for (var i = 0; i < 20001; i++) {
+         str = str + i +", ";
+     }
+     long stopTime = System.currentTimeMillis();
+     System.out.println("Длительность работы, в мс.: " + (stopTime - startTime));
+     System.out.println(str);
 
+
+     StringBuilder sb = new StringBuilder();
+     long startTime2 = System.currentTimeMillis();
+     for (var j = 0; j < 20001; j++) {
+         sb.append(j + ", ");
+     }
+        long stopTime2 = System.currentTimeMillis();
+        System.out.println("Длительность работы, в мс.: " + (stopTime2 - startTime2));
+        System.out.println(sb);
     }
 }
--- src/main/java/day8/Task2.java
+++ src/main/java/day8/Task2.java
@@ -2,6 +2,7 @@ package day8;
 
 public class Task2 {
     public static void main(String[] args) {
-
+     Airplane plane1 = new Airplane("Boeing", 2015, 70, 350);
+     System.out.println(plane1);
     }
 }
